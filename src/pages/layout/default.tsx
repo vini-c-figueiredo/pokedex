be714a5634ba269b115dbc3eb91d7e67dd87b2f5
Vinicius <viniciusfigueiredo.rp2@gmail.com>
2025-01-15 02:39:57 -0300

    Adjust flex items

--- src/pages/layout/default.tsx
+++ src/pages/layout/default.tsx
@@ -5,12 +5,12 @@ import { Outlet } from "react-router-dom";
 export function Default() {
     return (
         <div className="flex h-screen">
-            <div className="w-56 bg-gray-200">
+            <div className="w-60">
                 <SidebarProvider>
                     <Menu />
                 </SidebarProvider>
             </div>
-            <div className="flex-1 p-4">
+            <div className="flex-1 p-6">
                 <Outlet />
             </div>
         </div>
